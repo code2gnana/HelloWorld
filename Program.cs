9038c13131893789dab9d3202b1fdd825e451313
Gnaneshwara Babu <mail2gnana@yahoo.co.in>
2023-02-21 14:46:59 +1100

    Add README.md

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
